e1b4df7e7ae972df1df8501ff55b156b9c4adb2a
maly93 <mat.byra@gmail.com>
2017-01-28 14:54:33 +0100

    Add ReactivateWorkflowProcessor and Reactivate method in TaskWorkflowController

--- src/WebApi2Book.Web.Api/Controllers/V1/TaskWorkflowController.cs
+++ src/WebApi2Book.Web.Api/Controllers/V1/TaskWorkflowController.cs
@@ -19,16 +19,19 @@ namespace WebApi2Book.Web.Api.Controllers.V1
     {
         private readonly IStartTaskWorkflowProcessor _startTaskWorkflowProcessor;
         private readonly ICompleteTaskWorkflowProcessor _completeTaskWorkflowProcessor;
+        private readonly IReactivateTaskWorkflowProcessor _reactivateTaskWorkflowProcessor;
 
         public TaskWorkflowController(IStartTaskWorkflowProcessor startTaskWorkflowProcessor,
-            ICompleteTaskWorkflowProcessor completeTaskWorkflowProcessor)
+            ICompleteTaskWorkflowProcessor completeTaskWorkflowProcessor,
+            IReactivateTaskWorkflowProcessor reactivateTaskWorkflowProcessor)
         {
             _startTaskWorkflowProcessor = startTaskWorkflowProcessor;
             _completeTaskWorkflowProcessor = completeTaskWorkflowProcessor;
+            _reactivateTaskWorkflowProcessor = reactivateTaskWorkflowProcessor;
         }
 
         [HttpPost]
-        [Route("tasks/{taskId:long}/activations", Name ="StartTaskRoute")]
+        [Route("tasks/{taskId:long}/activations", Name = "StartTaskRoute")]
         public Task StartTask(long taskId)
         {
             var task = _startTaskWorkflowProcessor.StartTask(taskId);
@@ -36,10 +39,18 @@ namespace WebApi2Book.Web.Api.Controllers.V1
         }
 
         [HttpPost]
-        [Route("tasks/{taskId:long}/completions", Name ="CompleteTaskRoute")]
+        [Route("tasks/{taskId:long}/completions", Name = "CompleteTaskRoute")]
         public Task CompleteTask(long taskId)
         {
             var task = _completeTaskWorkflowProcessor.CompleteTask(taskId);
+            return task;
+        }
+
+        [HttpPost]
+        [Route("tasks/{taskId:long}/reactivations", Name = "ReactivateTaskRoute")]
+        public Task ReactivateTask(long taskId)
+        {
+            var task = _reactivateTaskWorkflowProcessor.ReactivateTask(taskId);
             return task;
         }
     }
---
+++ src/WebApi2Book.Web.Api/MaintenanceProcessing/ReactivateTaskWorkflowProcessor.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi2Book.Common;
+using WebApi2Book.Data.Exceptions;
+using WebApi2Book.Data.QueryProcessors;
+using WebApi2Book.Web.Api.Models;
+
+namespace WebApi2Book.Web.Api.MaintenanceProcessing
+{
+    public class ReactivateTaskWorkflowProcessor : IReactivateTaskWorkflowProcessor
+    {
+        private readonly IMapper _autoMapper;
+        private readonly IDateTime _dateTime;
+        private readonly ITaskByIdQueryProcessor _taskByIdQueryProcessor;
+        private readonly IUpdateTaskStatusQueryProcessor _updateTaskStatusQueryProcessor;
+
+        public ReactivateTaskWorkflowProcessor(ITaskByIdQueryProcessor taskByIdQueryProcessor,
+            IUpdateTaskStatusQueryProcessor updateTaskStatusQueryProcessor,
+            IMapper autoMapper, IDateTime dateTime)
+        {
+            _autoMapper = autoMapper;
+            _dateTime = dateTime;
+            _taskByIdQueryProcessor = taskByIdQueryProcessor;
+            _updateTaskStatusQueryProcessor = updateTaskStatusQueryProcessor;
+        }
+
+        public Task ReactivateTask(long taskId)
+        {
+            var taskEntity = _taskByIdQueryProcessor.GetTask(taskId);
+
+            if (taskEntity == null)
+            {
+                throw new RootObjectNotFoundException("Task not found");
+            }
+
+            //Simulate some workflow logic...
+            if (taskEntity.Status.Name != "Completed")
+            {
+                throw new BusinessRuleViolationException("Incorrect task status. Expected status of 'Completed'.");
+            }
+
+            taskEntity.CompletedDate = null;
+            _updateTaskStatusQueryProcessor.UpdateTaskStatus(taskEntity, "In Progress");
+
+            var task = _autoMapper.Map<Task>(taskEntity);
+            return task;
+        }
+    }
+}
