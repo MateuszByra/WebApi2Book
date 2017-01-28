441521378647ed1da9221dc1c703bd567cb56a9b
maly93 <mat.byra@gmail.com>
2017-01-28 14:30:26 +0100

    Add CompleteTaskWorkflowProcessor. Move Authorize attribute to the controller instead of method in TaskWorkflowController

---
+++ src/WebApi2Book.Web.Api/MaintenanceProcessing/CompleteTaskWorkflowProcessor.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi2Book.Common;
+using WebApi2Book.Data.QueryProcessors;
+using WebApi2Book.Web.Api.Models;
+
+namespace WebApi2Book.Web.Api.MaintenanceProcessing
+{
+    public class CompleteTaskWorkflowProcessor : ICompleteTaskWorkflowProcessor
+    {
+        private readonly IMapper _autoMapper;
+        private readonly ITaskByIdQueryProcessor _taskByIdQueryProcessor;
+        private readonly IDateTime _dateTime;
+        private readonly IUpdateTaskStatusQueryProcessor _updateTaskStatusQueryProccessor;
+
+        public CompleteTaskWorkflowProcessor(ITaskByIdQueryProcessor taskByIdQueryProcessor,
+            IUpdateTaskStatusQueryProcessor updateTaskStatusQueryProcessor,
+            IMapper autoMapper, IDateTime dateTime)
+        {
+            _autoMapper = autoMapper;
+            _taskByIdQueryProcessor = taskByIdQueryProcessor;
+            _dateTime = dateTime;
+            _updateTaskStatusQueryProccessor = updateTaskStatusQueryProcessor;
+        }
+
+        public Task CompleteTask(long taskId)
+        {
+            var taskEntity = _taskByIdQueryProcessor.GetTask(taskId);
+
+            if (taskEntity == null)
+            {
+                throw new BusinessRuleViolationException("Incorrect task status. Expected status of 'In Progress'.");
+            }
+
+            taskEntity.CompletedDate = _dateTime.UtcNow;
+            _updateTaskStatusQueryProccessor.UpdateTaskStatus(taskEntity, "Completed");
+
+            var task = _autoMapper.Map<Task>(taskEntity);
+            return task;
+        }
+    }
+}
