c0ef6b5237d7bbadf3f8d968805e4e63e0f1cc07
maly93 <mat.byra@gmail.com>
2017-03-10 18:50:07 +0100

    First integration test - get all tasks

---
+++ src/WebApi2Book.Web.Api.IntegrationTests/TasksControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi2Book.Web.Api.Models;
+using Task = WebApi2Book.Web.Api.Models.Task;
+
+namespace WebApi2Book.Web.Api.IntegrationTests
+{
+    [TestFixture]
+    public class TasksControllerTest
+    {
+        private WebClientHelper _webClientHelper;
+        private const string UriRoot = "http://localhost:65348/api/v1/";
+
+        [SetUp]
+        public void Setup()
+        {
+            _webClientHelper = new WebClientHelper();
+        }
+
+        [Test]
+        public void GetTasks()
+        {
+            var client = _webClientHelper.CreateWebClient();
+            try
+            {
+                const string address = UriRoot + "tasks";
+                var responseString = client.DownloadString(address);
+                var jsonResponse = JObject.Parse(responseString);
+                Assert.IsNotNull(jsonResponse.ToObject<PagedDataInquiryResponse<Task>>());
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+    }
+}
---
+++ src/WebApi2Book.Web.Api.IntegrationTests/WebClientHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi2Book.Common;
+
+namespace WebApi2Book.Web.Api.IntegrationTests
+{
+    public class WebClientHelper
+    {
+        public WebClient CreateWebClient(string username = "bhogg", string contentType=Constants.MediaTypeNames.TextJson)
+        {
+            var webClient = new WebClient();
+            var creds = username + ":" + "ignored";
+            var bcreds = Encoding.ASCII.GetBytes(creds);
+            var base64Creds = Convert.ToBase64String(bcreds);
+            webClient.Headers.Add("Authorization", "Basic " + base64Creds);
+            webClient.Headers.Add("Content-Type", contentType);
+            return webClient;
+        }
+    }
+}
